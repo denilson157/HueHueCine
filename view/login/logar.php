0489cb3e37933538174108b3b5cd9d4a41ccedff
Nicolas Alberto <darkathnva@gmail.com>
2020-10-26 23:46:59 -0300

    Adicionado Login com Banco de Dados

--- view/login/logar.php
+++ view/login/logar.php
@@ -1,4 +1,6 @@
 <?php
+require_once '../../config/database.php'; //Solicita Arquivo para conexão com banco
+
 $credentials = [
     0 => ['email' => 'deene67@gmail.com', 'senha' => '123456789'], //Usuario Denilson
     1 => ['email' => 'naranonva@hotmail.com', 'senha' => '123456789'] //Usuario Nicolas
@@ -13,7 +15,29 @@ else {
     $email = $_POST['email'];
     $senha = $_POST['password'];
 
-    if (in_array(['email' => $email, 'senha' => $senha], $credentials)) {
+    //Prepara a Query para buscar usuario no banco
+    $sql = $banco->prepare("SELECT email, senha FROM dbo.Usuario WHERE email = '".$email."' AND senha = '".$senha."' "); 
+    $sql->execute();
+
+    if($sql->rowCount() != 0){ //Verifica se teve retorno positivo
+
+        //Puxa os dados e passa para a sessão
+        $usuario = $sql->fetch();   //Puxa apenas 1 registro da consulta
+        $_SESSION['user'] = $usuario['email']; //Da para passar outros parametros, de acordo com retorno do SELECT
+
+        Header("Location: /view/home/");
+
+    }else{
+        
+        $msg = "Credenciais inválidas, tente novamente";
+        include './index.php';
+
+    }
+    
+
+    //Login com array estatico
+
+    /*if (in_array(['email' => $email, 'senha' => $senha], $credentials)) {
 
         $user = array_filter($credentials,  function ($user) {
             return $user['email'] == $_POST['email'];
@@ -25,5 +49,5 @@ else {
 
         $msg = "Credenciais inválidas, tente novamente";
         include './index.php';
-    }
+    }*/
 }
